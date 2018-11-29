9d26a25e383fe0b1a726682ce9b890beda595854
Amir K. Afshar <afshar@knights.ucf.edu>
2018-11-29 18:27:24 -0500

    Add files via upload

--- Intro Queues.cpp
+++ Intro Queues.cpp
@@ -4,21 +4,47 @@
 
 #include <queue>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void main()
 {
 	queue<int> Queue1;
+	queue<char> Queue2;
+
+	string str = "Hello";
 
 	Queue1.push(1);	// Push first element into queue
 	Queue1.push(2);
 	Queue1.push(3);
 	Queue1.push(4);	// Push last element into queue
-	cout << "First Element: " << Queue1.front() << endl;
+	cout << "Queue 1 First Element: " << Queue1.front() << endl;
 	
 	Queue1.pop();	// Pops first element in queue
 
-	cout << "First Element, after pop: " << Queue1.front() << endl;
-	cout << "Last Element: " << Queue1.back() << endl;
+	cout << "Queue 1 First Element, after pop: " << Queue1.front() << endl;
+	cout << "Queue 1 Last Element: " << Queue1.back() << endl;
+
+	Queue2.push(str[0]);	// Push first element into queue
+	Queue2.push(str[1]);
+	Queue2.push(str[2]);
+	Queue2.push(str[3]);
+	Queue2.push(str[4]);
+
+	cout << "Queue 2 First Element: " << Queue2.front() << endl;
+	cout << "Queue 2 Last Element: " << Queue2.back() << endl;
+
+	cout << endl << "Is Queue2 Empty?:" << endl;
+	
+	if (Queue2.empty())
+	{
+		cout << "Empty" << endl;
+	}
+	else
+	{
+		cout << "Not Empty." << endl;
+	}
+
+
 }
